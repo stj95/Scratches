aa890f59fad9be0b2f6cc2344dfcce55fbde4f7f
Stephen Jackson <stephen95.jackson@gmail.com>
2019-10-25 12:41:55 +0100

    added -esk for each stream functionality

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Quick scripts
 ## dir_create: 
 
 ### arguments: 
-    - -d str: Directory you want to add folders
-    - -l str: Folder name you want to add
+    * "-esk" str: Creates z2, n2, e2 stream folders  
+    * "-d" str: Directory you want to add folders
+    * "-l" str: Folder name you want to add
     
 ### returns:
-    - Creates a folder for each -l argument, in the -d directory
+    * Creates a folder for each -l argument, in the -d directory
     
 Note: you need a "-l" before every folder name
--- dir_create.py
+++ dir_create.py
@@ -2,12 +2,16 @@ import argparse
 import os
 
 
-def make_folders(home_path, new_folders):
+def make_folders(home_path, new_folders, eskdalemuir):
+
 
     for folder in new_folders:
         try:
-            for suffix in ["z2", "n2", "e2"]:
-                os.mkdir(home_path + "\\" + folder + suffix)
+            if eskdalemuir == True:
+                for suffix in ["z2", "n2", "e2"]:
+                    os.mkdir(home_path + "\\" + folder + suffix)
+            else:
+                os.mkdir(home_path + "\\" + folder)
 
         except FileExistsError:
             continue
@@ -16,11 +20,14 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('-esk', '--eskdalemuir', action="store_true",
+                            help='If given, creates z2, e2, n2 streams for each folder')
+
     parser.add_argument('-d', '--directory', help='The directory you want to create folders in')
     parser.add_argument('-l', '--folder_list', action='append')
 
     args = parser.parse_args()
     print(args)
-    make_folders(args.directory, args.folder_list)
+    make_folders(args.directory, args.folder_list, args.eskdalemuir)
 
 
